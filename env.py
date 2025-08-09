bd737882f6ff9aaff5988f334c6aafbc788438e6
Anthony <91166746+anthonyjV1@users.noreply.github.com>
2025-08-09 19:21:58 -0400

    .

--- env.py
+++ env.py
@@ -1,7 +1,10 @@
 import numpy as np
+import threading
+import time
 from actions import Actions
 from card_detection import CardDetection
 from troop_detection import TroopDetection
+
 
 MAX_ALLIES = 10
 MAX_ENEMIES = 10
@@ -11,6 +14,28 @@ class Env:
         self.actions = Actions()
         self.troop_detection = TroopDetection()
         self.card_detection = CardDetection()
+
+        self.num_cards = 4
+        self.grid_width = 18
+        self.grid_height = 28
+
+        self.game_end_flag = None
+        self._game_end_thread = None
+        self._game_end_thread_stop = threading.Event()
+    
+    def reboot(self):
+        time.sleep(3)
+        self.game_end_flag = None
+        self._game_end_thread_stop.clear()
+        self._game_end_thread = threading.Thread(target=self._game_end_detector, daemon=True)
+        self._game_end_thread.start()
+        state = self._get_state()
+        return state
+    
+    def finish(self):
+        self._game_end_thread_stop.set()
+        if self._game_end_thread:
+            self._game_end_thread.join()
     
     def _get_state(self):
         elixir = self.actions.count_elixir()
@@ -40,6 +65,7 @@ class Env:
         state = np.array(cards_in_hand + [elixir / 10] + final_ally + final_enemy, dtype=np.float32)
         return state
 
+
     def _compute_reward(self, state):
         if state is None:
             return 0
@@ -63,3 +89,21 @@ class Env:
         self.prev_enemy_presence = enemy_presence
 
         return reward
+
+    def get_all_actions(self):
+        actions = [
+            [card, x / (self.grid_width - 1), y / (self.grid_height - 1)]
+            for card in range(self.num_cards)
+            for x in range(self.grid_width)
+            for y in range(self.grid_height)
+        ]
+        actions.append([-1, 0, 0])  # No-op action
+        return actions
+
+    def _game_end_detector(self):
+        while not self._game_end_thread_stop.is_set():
+            result = self.actions.detect_winner()
+            if result:
+                self.game_end_flag = result
+                break
+        time.sleep(1)
