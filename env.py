cc1015fa81d6a00b0a59b1b993badc82aacf60e9
Anthony <91166746+anthonyjV1@users.noreply.github.com>
2025-08-10 14:47:19 -0400

    lol

--- env.py
+++ env.py
@@ -24,7 +24,7 @@ class Env:
         self.game_end_flag = None
         self._game_end_thread = None
         self._game_end_thread_stop = threading.Event()
-        
+
         self.prev_elixir = None
         self.prev_enemy_presence = None
         self.prev_enemy_tower = None
